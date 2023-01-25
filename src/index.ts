d86546f316ebd685ddd57f9a49bd995515c9d6a2
Rufi <Rafajos9@gmail.com>
2023-01-25 12:44:06 -0400

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import books from './routes/books'
 import morgan from 'morgan'
+import cors from 'cors';
 const app = express()
 
+//Settings
+app.set('PORT',3000 || process.env.PORT)
+
+//middlewares
 app.use(express.json()) //req.body to json
 app.use(morgan('dev'))
-app.set('PORT',3000 || process.env.PORT)
+app.use(cors())
 
 //Routes
 app.use('/api/books',books)
